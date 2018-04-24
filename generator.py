05e0426541b9ccf291e4198cc77e8c2a9ac55d1f
Zhiyuan Wang <wangzyzju@gmail.com>
2018-04-23 23:13:58 -0700

    Use base64 with len

--- generator.py
+++ generator.py
@@ -1,9 +1,12 @@
 import hashlib
+import base64
+
+LEN = 26
 
 with open("seed.txt", "r") as f:
     lines = list(f.readlines())
     salt, lines = lines[0], lines[1:]
     for line in lines:
         salt = hashlib.sha224(salt).hexdigest()
-        newpwd = hashlib.sha224(line + salt).hexdigest()
-        print line + " -> " + newpwd
+        newpwd = base64.b64encode(hashlib.sha224(line + salt).hexdigest())
+        print line[:-1] + " -> " + newpwd[:LEN]
